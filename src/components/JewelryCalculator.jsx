8397f21bd84220f8f8174a45312a32bbaca3ebcd
pixiegirl32 <crestedbuttejewelry@gmail.com>
2025-03-15 16:53:21 -0600

    Update JewelryCalculator.jsx

--- src/components/JewelryCalculator.jsx
+++ src/components/JewelryCalculator.jsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from './components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
 import { Calculator } from 'lucide-react';
 import * as XLSX from 'xlsx';
 import jsPDF from 'jspdf';
