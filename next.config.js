29da7d4d9b8d57dcbb5a8228d189caedbc6e3570
pixiegirl32 <crestedbuttejewelry@gmail.com>
2025-03-15 16:10:20 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
-  // This will help with localStorage issues during server-side rendering
-  experimental: {
-    appDir: true,
-  },
 }
 
 module.exports = nextConfig
